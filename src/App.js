d7f488184c87ddc86ff32d8489713edf53fbf026
Hunter-137 <asrarkulovamal@gmail.com>
2024-11-02 18:47:21 +0500

    feat: add styles for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import Counter from "./components/Counter";
-import InputText from "./components/InputText";
+// import Counter from "./components/Counter";
+// import InputText from "./components/InputText";
+import "./styles/Reset.css";
+import "./styles/App.css";
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
-      <InputText></InputText>
+      {/* <Counter></Counter>
+      <InputText></InputText> */}
+
+      <div className="wrapper">
+        <div className="list">
+          <div className="item">
+            <div className="item__text">
+              <h2 className="titles">1. Javascript</h2>
+              <p className="descs"> Javascript — это язык программирования</p>
+            </div>
+            <div className="item__btn">
+              <button className="descs">Удалить</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
